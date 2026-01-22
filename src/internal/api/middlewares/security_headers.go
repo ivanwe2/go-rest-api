93fecc2be9ceaf2c40edff0ff85d710120aeb230
Ivan Rusev <Ivan.Rusev@indeavr.com>
2026-01-22 11:28:49 +0200

    feat: additional security headers

--- src/internal/api/middlewares/security_headers.go
+++ src/internal/api/middlewares/security_headers.go
@@ -4,14 +4,39 @@ import "net/http"
 
 func SecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-DNS-Prefetch-Control", "off")
-		w.Header().Set("X-Frame-Options", "off")
 		w.Header().Set("X-XSS-Protection", "1;mode=block")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("Strict Transport Security", "max-age=63072000;includeSubdomains;preload")
+		w.Header().Set("Strict-Transport-Security", "max-age=63072000;includeSubdomains;preload")
 		w.Header().Set("Content-Security-Policy", "default-src 'self'")
-		w.Header().Set("Referred-Policy", "no-referrer")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+
+		// Strategy: Using "express" as a decoy
 		w.Header().Set("X-Powered-By", "express")
+
+		// Additional security headers
+		w.Header().Set("Server", "")
+		w.Header().Set("X-Permitted-Cross-Domain-Policies", "none")
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("Pragma", "no-cache")
+
+		// Isolation Headers
+		w.Header().Set("Cross-Origin-Resource-Policy", "same-origin")
+		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+
+		// CORS Headers
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		w.Header().Set("Permissions-Policy", "geolocation=(self), microphone=()")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
